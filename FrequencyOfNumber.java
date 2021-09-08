cde8a69a61762846dac944f7cb8e776d342670b4
PaviArthi <90091549+PaviArthi@users.noreply.github.com>
2021-09-08 19:01:13 +0530

    Add files via upload

---
+++ FrequencyOfNumber.java
@@ -0,0 +1,30 @@
+package assignments;
+
+public class FrequencyOfNumber {
+
+	public static void main(String[] args) {
+		int [] arr = new int [] {3,4,2,5,1,2,6,3,6,2,4,7,1};           
+        int [] fr = new int [arr.length];  
+        int visited = -1;  
+        for(int i = 0; i < arr.length; i++){  
+            int count = 1;  
+            System.out.println("Frequency of an Element");
+            for(int j = i+1; j < arr.length; j++){  
+                if(arr[i] == arr[j]){  
+                    count++;  
+                    fr[j] = visited;  
+                }  
+            }  
+            if(fr[i] != visited)  
+                fr[i] = count;  
+        }         
+        for(int i = 0; i < fr.length; i++){  
+            if(fr[i] != visited)  
+                System.out.println("    " + arr[i] + " occured " + fr[i]+" times");  
+        }  
+        System.out.println("----------------------------------------");  
+   
+
+	}
+
+}
